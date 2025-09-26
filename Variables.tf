cd6f9fd0bf2bcc8fdb38d2865800d44f46042713
niteen-git <niteen.batavle@gmail.com>
2025-09-26 14:39:19 +0530

    updated

---
+++ Variables.tf
@@ -0,0 +1,18 @@
+# Define variables
+variable "snowflake_account_name" {
+  description = "Snowflake Account"
+  type        = string
+  default     = "mk14271"
+}
+
+variable "snowflake_username" {
+ description = "Snowflake Username"
+  type        = string
+  default     = "SNOWNITEEN"
+}
+
+variable "snowflake_password" {
+ description = "Snowflake Password"
+  type        = string
+  default     = "Snowflake@2025"
+}
---
+++ provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    snowflake = {
+      source  = "Snowflake-Labs/snowflake"
+      version = "~> 0.76"
+    }
+  }
+}
+
+provider "snowflake" {
+  username = var.snowflake_username
+  password = var.snowflake_password
+  account  = var.snowflake_account_name
+  region   = var.snowflake_region
+}
